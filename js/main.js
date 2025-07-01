4758940e9bee0cd62a06de9fbd7146d3ac601287
Itaish-Bzu <Itaish94@gmail.com>
2025-07-01 16:09:09 +0300

    Change ball color

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 var size = 100
 
 function onBallClick() {
-  size += getRandomInt(20,60)
-  console.log(size );
-    if (size >= 400) {
+  size += getRandomInt(20, 60)
+  const color = getRandomColor()
+  if (size > 400) {
     size = 100
   }
   const elBall = document.querySelector('.ball')
   elBall.style.height = size + 'px'
   elBall.style.width = size + 'px'
   elBall.innerText = size
-
+  elBall.style.backgroundColor = color
 }
