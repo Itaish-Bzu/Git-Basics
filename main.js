38ddad66033c1a2e3f4d2e073538d5e63ea28f11
Itaish-Bzu <Itaish94@gmail.com>
2025-07-01 13:45:47 +0300

    Change ball size

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 'use strict'
-function onBallClick(){
-    console.log("Hi");
-    
+var size = 100 
+
+function onBallClick() {
+  size += 50
+  const elBall = document.querySelector('.ball')
+  elBall.style.height = size +'px'
+  elBall.style.width = size +'px'
+  elBall.innerText = size 
+  
 }
